ae9ac2d5d1c886bc4c4478678e81bb38d9ee05b4
unknown <kakha.putkaradze@eu.edu.ge>
2021-07-11 18:46:52 +0400

    HR-EU-KP

--- HR-EU-KP/Hr.js
+++ HR-EU-KP/Hr.js
@@ -145,6 +145,33 @@ function myFunction9() {
     
   function closeNav() {
       document.getElementById("Navigation").style.width = "0";}
+
+    $(function() {
+      var Accordion = function(el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+  
+      var links = this.el.find('.link');
+  
+      links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+      }
+  
+      Accordion.prototype.dropdown = function(e) {
+      var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+  
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+  
+      if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+      };
+      }
+  
+      var accordion = new Accordion($('#accordion'), false);
+      });
+      
 //  HR dropdown
   function menuToggle(){
     const toggleMenu = document.querySelector('.menu');
